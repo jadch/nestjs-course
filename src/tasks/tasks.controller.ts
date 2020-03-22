7b878cb712898b206a52104b9d7405a737d8f4e4
Jad ch <jad.chalita@gmail.com>
2020-03-22 12:36:37 +0100

    Created our first request : get all tasks at GET /tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
+/**
+ * The controller handles the HTTP requests and returns the response.
+ * The service handles the business logic.
+ */
 @Controller('tasks')
 export class TasksController {
-  constructor(private TasksService: TasksService) {}
+  constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
 }
